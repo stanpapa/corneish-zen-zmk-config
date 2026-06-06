486f953a30c004ed6e798fcd0ca2b75e3c8f7832
Stan Papadopoulos <stan.papadopoulos@proton.me>
2026-06-06 13:57:03 +0200

    simplify config for debugging

--- config/custom/custom.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#define U_NP &none // key is not present
-#define U_NA &none // present but not available for use
-#define U_NU &none // available but not used
-
-#define U_TAPPING_TERM 200
-
-#include "behaviors.h"
